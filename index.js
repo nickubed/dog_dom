1ff8c65370a6bbaf0703371c8d0964290df23c3b
nickubed <nhpquandt@gmail.com>
2020-03-15 13:25:22 -0700

    Initial branch

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-var dog = document.getElementById('dogPic')
-var music = document.getElementById('ruffRyderAnthem')
-var words = document.getElementById('dogText')
-
-function ruff(){
-    music.play()
-    words.style.color = 'pink'
-}
-
-document.addEventListener('DOMContentLoaded', ()=> {
-    dog.addEventListener('click', ruff)
-})
+// We'll add code here!
